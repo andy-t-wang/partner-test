08e6d9e43f7ca7e85f1ee67626a4d760574c48dc
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2025-02-20 22:50:28 -0800

    remove

--- app/api/verify/route.ts
+++ app/api/verify/route.ts
@@ -1,13 +1,13 @@
 export const dynamic = "force-dynamic";
-import { verifyCloudProof } from "@worldcoin/idkit";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   console.log("here");
   const body = await req.json();
   const { proof, action, signal } = body;
-  const result = await verifyCloudProof(proof, action, signal);
-  console.log(result);
-  // TODO: Verify Proof
-  return NextResponse.json(result);
+  console.log(proof, action, signal);
+  // const result = await verifyCloudProof(proof, action, signal);
+  // console.log(result);
+  // TODO:
+  return NextResponse.json({ success: true });
 }
